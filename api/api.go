61a3b5debd1e400af499889008c270e706788b9f
Lucas Lins <lucas.fsl.92@gmail.com>
2025-02-26 22:53:38 -0300

    feature: implementing post, show and index routes

--- api/api.go
+++ api/api.go
@@ -3,16 +3,15 @@ package api
 import (
 	"encoding/json"
 	"log/slog"
+	"math/rand/v2"
 	"net/http"
-
+	"net/url"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-
-
-
 func sendJson(w http.ResponseWriter, response Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
 	data, err := json.Marshal(response)
 	if err != nil {
 		slog.Error("error in JSON marshal", "error", err)
@@ -25,7 +24,7 @@ func sendJson(w http.ResponseWriter, response Response, status int) {
 	}
 }
 
-func NewHandler() http.Handler {
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Logger)
@@ -34,22 +33,59 @@ func NewHandler() http.Handler {
 	r.Use(middleware.RealIP)
 
 	r.Route("/api", func(r chi.Router) {
-		r.Post("/short", handlePost)
-		r.Get("/{code}", handleGet)
+		r.Post("/short", handlePost(db))
+		r.Get("/{code}", handleGet(db))
+		r.Get("/", handleIndex(db))
 	})
-	
 
 	return r
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request){
-
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJson(w, Response{Error: "invalid body"}, http.StatusUnprocessableEntity)
+			return
+		}
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJson(w, Response{Error: "invalid URL"}, http.StatusBadRequest)
+			return
+		}
+		code := getCode()
+		db[code] = body.URL
+		sendJson(w, Response{Data: code}, http.StatusCreated)
+	}
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
-	// code := chi.URLParam(r, "code")
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var code = chi.URLParam(r, "code")
+		data, ok := db[code]
+		if !ok {
+			sendJson(w, Response{Error: "code not found"}, http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, data, http.StatusPermanentRedirect)
+	}
 }
 
+func handleIndex(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sendJson(w, Response{Data: db}, http.StatusOK)
+	}
+}
+
+func getCode() string {
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	const limit = 8
+	bytes := make([]byte, limit)
+
+	for i := range limit {
+		bytes[i] = characters[rand.IntN(len(characters))]
+	}
+	return string(bytes)
+}
 
 type PostBody struct {
 	URL string `json:"url"`
@@ -57,5 +93,5 @@ type PostBody struct {
 
 type Response struct {
 	Error string `json:"error,omitempty"`
-	Data any `json:"data,omitempty"`
+	Data  any    `json:"data,omitempty"`
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	if err := run(); err !=nil {
+	if err := run(); err != nil {
 		slog.Error("error in server", "error", err)
 		return
 	}
@@ -17,14 +17,15 @@ func main() {
 
 }
 
-func run() error{
-	handler := api.NewHandler()
+func run() error {
+	fakeDb := make(map[string]string)
+	handler := api.NewHandler(fakeDb)
 	server := http.Server{
-		ReadTimeout: 10 * time.Second,
-		IdleTimeout: time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Second,
 		WriteTimeout: 10 * time.Second,
-		Addr: ":8080",
-		Handler: handler,
+		Addr:         ":8080",
+		Handler:      handler,
 	}
 	if err := server.ListenAndServe(); err != nil {
 		return err
